a01a8aa6fca2d918b97ceba41fb55ba39034f8f4
shrfrn <shrfrn@gmail.com>
2024-08-27 16:47:40 +0300

    Some cleanup

--- js/services/wikitube.service.js
+++ js/services/wikitube.service.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const YT_API_KEY = 'AIzaSyB_6u19ZnSR_5zv7HYgTJKw6qkPpnsREcg'
+
 const WIKIS_STORAGE_KEY = 'wikisDB'
 const VIDEOS_STORAGE_KEY = 'videosDB'
 
@@ -11,52 +12,53 @@ const ytURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResu
 const wikiURL = 'https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&format=json'
 
 function getVideos(keyword) {
-  if (gVideoMap[keyword]) {
-    return Promise.resolve(gVideoMap[keyword])
-  }
-  
-  return axios.get(`${ytURL}&q=${keyword}`).then(({ data }) => {
-    gVideoMap[keyword] = data.items.map(_getVideoInfo)
-    saveToStorage(VIDEOS_STORAGE_KEY, gVideoMap)
-    return gVideoMap[keyword]
-  })
+	if (gVideoMap[keyword]) {
+		return Promise.resolve(gVideoMap[keyword])
+	}
 
+	return axios.get(`${ytURL}&q=${keyword}`)
+        .then(({ data }) => {
+            gVideoMap[keyword] = data.items.map(_getVideoInfo)
+            saveToStorage(VIDEOS_STORAGE_KEY, gVideoMap)
+            return gVideoMap[keyword]
+        })
 }
 
-function _getWikiInfo({title, snippet}) {
-  return {title, snippet}
+function _getWikiInfo({ title, snippet }) {
+	return { title, snippet }
 }
 
+function _getVideoInfo(video) {
+	const { id, snippet } = video
+	const { title, thumbnails } = snippet
 
-  function _getVideoInfo(video) {
-  const { id, snippet } = video
-  const { title, thumbnails } = snippet
-  const videoId = id.videoId
-  const thumbnail = thumbnails.default.url
-  return { id: videoId, title, thumbnail }
+	const videoId = id.videoId
+	const thumbnail = thumbnails.default.url
+	
+    return { id: videoId, title, thumbnail }
 }
 
 function getWiki(keyword) {
-  
-  if (gWikiMap[keyword]) {
-    return Promise.resolve(gWikiMap[keyword])
-  }
+	if (gWikiMap[keyword]) {
+		return Promise.resolve(gWikiMap[keyword])
+	}
 
-  return axios.get(`${wikiURL}&srsearch=${keyword}`).then(({data}) => {
-    gWikiMap[keyword] = data.query.search.slice(0, 3).map(_getWikiInfo)
-    saveToStorage(WIKIS_STORAGE_KEY, gWikiMap)
-    return gWikiMap[keyword]
-  })
-
+	return axios.get(`${wikiURL}&srsearch=${keyword}`)
+        .then(({ data }) => {
+            gWikiMap[keyword] = data.query.search.slice(0, 3).map(_getWikiInfo)
+            saveToStorage(WIKIS_STORAGE_KEY, gWikiMap)
+            return gWikiMap[keyword]
+        })
 }
 
 function clearHistory() {
-  gVideoMap = {}
-  gWikiMap = {}
-  saveToStorage(VIDEOS_STORAGE_KEY, gVideoMap)
-  saveToStorage(WIKIS_STORAGE_KEY, gWikiMap)
+	gVideoMap = {}
+	gWikiMap = {}
+    
+	saveToStorage(VIDEOS_STORAGE_KEY, gVideoMap)
+	saveToStorage(WIKIS_STORAGE_KEY, gWikiMap)
 }
 
 function getKeywords() {
-  return Object.keys(gVideoMap)
+	return Object.keys(gVideoMap)
 }
--- js/wikitube.controller.js
+++ js/wikitube.controller.js
@@ -1,122 +1,110 @@
-// Needed for Modal Bonus
-var gKeepResolve
+'use strict'
+
+var gKeepResolve // Needed for Modal Bonus
 
 function onInit() {
-  const txt = 'Micky Mouse'
-  document.querySelector('.search-input').value = txt
-  searchAndRender(txt)
+	const txt = 'Micky Mouse'
+	document.querySelector('.search-input').value = txt
+	searchAndRender(txt)
 }
 
 function searchAndRender(txt) {
-  //Youtube
-  getVideos(txt)
-    .then((videos) => {
-      const selectedVideoId = videos[0].id
-      setVideoPlayer(selectedVideoId)
-      renderVideos(videos)
-      renderHistory()
-    })
+	//Youtube
+	getVideos(txt).then(videos => {
+		const selectedVideoId = videos[0].id
+		loadVideo(selectedVideoId)
+		renderVideoList(videos)
+		renderHistory()
+	})
 
-  //Wiki
-  getWiki(txt).then((wikis) => {
-    renderWikis(wikis)
-  })
-
+	//Wiki
+	getWiki(txt).then(renderWikis)
 }
 
-function renderVideos(videos) {
-  const strHTMLs = videos.map(({ id, title, thumbnail }) => {
-    return `
-      <article onclick="onChangeVideo('${id}')" class="video-card">
-          <img class="video-thumbnail" src="${thumbnail}" alt="">
-          <h3 class="video-title">${title}</h3>
-      </article>`
-  })
-  document.querySelector('.videos-container').innerHTML = strHTMLs.join('')
+function renderVideoList(videos) {
+	const strHTMLs = videos.map(({ id, title, thumbnail }) => {
+		return `
+            <article onclick="onChangeVideo('${id}')" class="video-card">
+                <img class="video-thumbnail" src="${thumbnail}" alt="">
+                <h3 class="video-title">${title}</h3>
+            </article>`
+	})
+	document.querySelector('.videos-container').innerHTML = strHTMLs.join('')
 }
 
-function setVideoPlayer(videoId) {
-  document.querySelector('.main-video iframe').src = `https://www.youtube.com/embed/${videoId}`
+function loadVideo(videoId) {
+	document.querySelector('.main-video iframe').src = `https://www.youtube.com/embed/${videoId}`
 }
 
 function renderWikis(wikis) {
-  const strHTMLs = wikis.map((wiki) => {
-    return `
+	const strHTMLs = wikis.map(wiki => {
+		const url = `https://en.wikipedia.org/wiki/${encodeURIComponent(wiki.title)}`
+		return `
             <article class="wiki-result">
-                <h3>
-                  <a href="https://en.wikipedia.org/wiki/${encodeURIComponent(wiki.title)}" target="_blank">
-                    ${wiki.title}
-                  </a>
-                </h3>
-                <p>
-                    ...${wiki.snippet}...
-                </p>
-            
+                <h3><a href=${url} target="_blank">${wiki.title}</a></h3>
+                <p>...${wiki.snippet}...</p>
             </article>`
-  })
-
-  document.querySelector('.wiki-container').innerHTML = strHTMLs.join('')
+	})
+	document.querySelector('.wiki-container').innerHTML = strHTMLs.join('')
 }
 
 function onChangeVideo(videoId) {
-  setVideoPlayer(videoId)
+	loadVideo(videoId)
 }
 
 function onSearch(ev) {
-  ev.preventDefault()
-  const txt = document.querySelector('.search-input').value
-  if (!txt) return
-  searchAndRender(txt)
+	ev.preventDefault()
+	
+    const txt = document.querySelector('.search-input').value
+	if (!txt) return
+
+	searchAndRender(txt)
 }
 
 function renderHistory() {
-  const keywords = getKeywords()
-  let strHTMLs = keywords
-    .map(
-      keyword =>
-        `<span>${keyword}</span>`
-    )
+	const keywords = getKeywords()
 
-  if (keywords.length === 0) strHTMLs = 'No keywords to show'
+	if (keywords.length === 0) var strHTMLs = 'No keywords to show'
+	else strHTMLs = keywords.map(keyword => `<span>${keyword}</span>`)
 
-  document.querySelector('.keywords-container').innerHTML = strHTMLs
+	document.querySelector('.keywords-container').innerHTML = strHTMLs
 }
 
 function onClearHistory() {
-  doConfirm().then(userRes => {
-    if (userRes) {
-      clearHistory()
-      renderHistory()
-    }
-  })
+	doConfirm().then(userRes => {
+		if (userRes) {
+			clearHistory()
+			renderHistory()
+		}
+	})
 }
 
 function onOpenThemeModal() {
-  askUser().then(color => {
-    document.querySelector('body').style.backgroundColor = color
-  })
+	askUser().then(color => {
+		document.querySelector('body').style.backgroundColor = color
+	})
 }
 
 function askUser() {
-  document.querySelector('.modal').hidden = false
-  return new Promise((resolve) => {
-    gKeepResolve = resolve
-  })
+	document.querySelector('.modal').hidden = false
+	return new Promise(resolve => {
+		gKeepResolve = resolve
+	})
 }
 
 function onChangeTheme() {
-  const color = document.querySelector('.change-theme').value
-  gKeepResolve(color)
-  document.querySelector('.modal').hidden = true
+	const color = document.querySelector('.change-theme').value
+	gKeepResolve(color)
+	document.querySelector('.modal').hidden = true
 }
 
 function onCloseModal() {
-  document.querySelector('.modal').hidden = true
+	document.querySelector('.modal').hidden = true
 }
 
-// For now wrapping the built-in confirm with a promise, 
+// For now wrapping the built-in confirm with a promise,
 // later we can use our own modal
 function doConfirm(title = 'sure?') {
-  const res = confirm(title)
-  return Promise.resolve(res)
+	const res = confirm(title)
+	return Promise.resolve(res)
 }
